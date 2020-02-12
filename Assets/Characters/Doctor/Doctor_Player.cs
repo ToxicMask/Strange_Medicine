4d856170183c804efe67196aa18dc9a8e8517165
ToxicMask <tomas.carciofi@gmail.com>
2020-02-12 00:57:22 -0300

    Placeholder Walk Animation

--- Assets/Characters/Doctor/Doctor_Player.cs
+++ Assets/Characters/Doctor/Doctor_Player.cs
@@ -15,6 +15,9 @@ public class Doctor_Player : MonoBehaviour
 
     const float pixel_aspect = 32f;
     public float walk_speed = 1f;
+
+    //Sight Raycast
+    Vector2 vector_raycast = Vector2.down;
     
     //private Vector3 linear_velocity = Vector3.zero;
 
@@ -32,24 +35,36 @@ public class Doctor_Player : MonoBehaviour
     // Movement and Input
     private void FixedUpdate()
     {
-        Pixel_Perfect_Movement(pixel_aspect);
+
+        // Get movement direction
+        Vector2 input_dir = Direction_Movement_Input();
+
+
+        // Pixel Perfect Movement
+        Pixel_Perfect_Movement(input_dir, pixel_aspect);
     }
 
-    void Pixel_Perfect_Movement(float pixel_unit)
+    void Pixel_Perfect_Movement(Vector2 current_dir, float pixel_unit)
     {
-        // New velocity based on direction of player input
-        Vector2 current_velocity = Float2PixelPerfect(Direction_Movement() * walk_speed, pixel_unit) * Time.fixedDeltaTime;
 
-        //Set current velocity
-        kinematic2D.velocity = current_velocity;
+        // New velocity based on direction of player input
+        Vector2 current_velocity = current_dir * walk_speed;
+
+        // Convert velocity to pixel perfect
+        current_velocity = Float2PixelPerfect(current_velocity, pixel_unit) ;
+
+        //Set current velocity based on delta time
+        kinematic2D.velocity = current_velocity * Time.fixedDeltaTime;
         
 
     }
 
-    Vector2 Direction_Movement()
+    Vector2 Direction_Movement_Input()
     {
         // Create direction for player movement
         Vector2 accel_dir = Vector3.zero;
+
+        // New Direction for Walk Animation
         WALK_DIR new_anim = WALK_DIR.IDLE;
 
 
@@ -66,7 +81,18 @@ public class Doctor_Player : MonoBehaviour
         else if (Input.GetKey(KeyCode.D))
         { accel_dir.x += 1; new_anim = WALK_DIR.RIGHT; }
 
-        //if (new_anim != WALK_DIR.IDLE) anim_control.SetInteger("Walk_Dir", (int)new_anim); anim_control.speed = 1; }else {  }
+        //Set new animation, Freze animation if Char is idle <TEMP>
+        anim_control.SetInteger("Dir_ID", (int)new_anim);
+
+        if (new_anim != WALK_DIR.IDLE)
+        {
+             anim_control.speed = 1;
+        }
+        else
+        {
+            //Freze animation
+            anim_control.speed = 0;
+        }
 
         return accel_dir;
     }
