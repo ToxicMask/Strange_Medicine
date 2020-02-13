cb1916abfb80439079674270885518b3955d1ce4
ToxicMask <tomas.carciofi@gmail.com>
2020-02-13 00:43:05 -0300

    Box Animation + Item use

--- Assets/ItemBox/ItemBox.cs
+++ Assets/ItemBox/ItemBox.cs
@@ -5,17 +5,32 @@ using UnityEngine;
 public class ItemBox : MonoBehaviour
 {
 
-    public Item stored_item = new Item(0);
+    public Animator anim_control;
+
+    public Item stored_item;
+    public int item_id = -1;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        anim_control = GetComponent<Animator>();
+
+        if (item_id > -1)
+        {
+            stored_item = new Item(item_id);
+        }
     }
 
 
     public Item Open_Box()
     {
+        anim_control.SetBool("Opened", true);
         return stored_item;
+    }
+
+    public void Close_Box()
+    {
+        anim_control.SetBool("Opened", false);
     }
 }
